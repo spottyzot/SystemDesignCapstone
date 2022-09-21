3c3185e6ea0b8a4d71c10ec53e84776b5afc0fc0
Shawn <sro1115@outlook.com>
2022-09-21 12:42:13 -0700

    rdy

--- server/connection.js
+++ server/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config;
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
-  database: process.env.PGDATABASE
+  database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
